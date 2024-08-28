95cb5a76260806a8e6f8300606223b1063ad003d
John Francis <john9francis@gmail.com>
2024-08-28 16:34:36 -0400

    Added button widget, and started messing with the text state

--- lib/button.dart
+++ lib/button.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
 
+class TextWidget extends StatefulWidget {
+
+  const TextWidget({super.key});
+
+  @override
+  _TextState createState() => _TextState();
+}
+
+class _TextState extends State<TextWidget> {
+  var number = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(number.toString()),
+    );
+  }
+}
+
 class MyButton extends StatelessWidget {
   const MyButton({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () => print("Hey"),
-
+      child: Container(
+        height: 50,
+        padding: const EdgeInsets.all(8),
+        margin: const EdgeInsets.symmetric(horizontal: 8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          color: Colors.lightGreen[500],
+        ),
+        child: const Center(
+          child: Text('Engage'),
+        ),
+      ),
     );
   }
 }
