9d057f86a7bbdeaed292c076c24b0e31e5d84024
Francis <francis.john@ufl.edu>
2024-08-30 14:21:21 -0400

    Made a few classes to streamline the main file, about to implement some of them but it might break

--- lib/main.dart
+++ lib/main.dart
@@ -24,11 +24,64 @@ void main() {
   );
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const appTitle = "GECAT";
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(appTitle),
+          ),
+        body: const Center(
+          child: Padding(
+            padding: EdgeInsets.all(180.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                MyTextField(hintText: 'Enter Height'),
+                MyTextField(hintText: 'Enter Weight'),
+                MyTextField(hintText: 'Enter Birth Gender')
+              ],
+            )
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyForm extends StatelessWidget {
+  const MyForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Padding(
+        padding: EdgeInsets.all(180.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            MyTextField(hintText: 'Enter Height'),
+            MyTextField(hintText: 'Enter Weight'),
+            MyTextField(hintText: 'Enter Birth Gender')
+          ],
+        )
+      ),
+    );
+  }
+}
+
 class MyTextField extends StatelessWidget {
   final String hintText;
 
   // Constructor to accept hintText
-  const MyTextField({required this.hintText});
+  const MyTextField({required this.hintText, super.key});
 
   @override
   Widget build(BuildContext context) {
